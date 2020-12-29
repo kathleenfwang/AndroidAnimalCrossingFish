ee8965f5d4f9fe7e0c673e64667801327e5b1d7f
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-28 20:53:39 -0800

    fish and availability class

--- app/src/main/java/com/kathleenwang/animalcrossingfish/Fish.kt
+++ app/src/main/java/com/kathleenwang/animalcrossingfish/Fish.kt
@@ -1,7 +1,18 @@
 package com.kathleenwang.animalcrossingfish
 
-class Fish(val name: String, val price: Int, val availability: String, val imageSrc: String) {
+import org.json.JSONObject
+
+class Fish(val name: String, val price: Int, var availability: Availability, val imageSrc: String) {
+
     override fun toString(): String {
         return "Fish(name='$name', price=$price, availability='$availability', imageSrc='$imageSrc')"
     }
+}
+
+class Availability(val months:String,val location:String,val rarity:String) {
+    override fun toString(): String {
+        return "Availability(months='$months', location='$location', rarity='$rarity')"
+    }
+
+
 }
--- app/src/main/java/com/kathleenwang/animalcrossingfish/MainActivity.kt
+++ app/src/main/java/com/kathleenwang/animalcrossingfish/MainActivity.kt
@@ -16,17 +16,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mainText = findViewById<TextView>(R.id.mainText)
-        flickrTask().execute()
+        fishTask().execute()
     }
-    inner class flickrTask(): AsyncTask<String, Void, String>() {
+    inner class fishTask(): AsyncTask<String, Void, String>() {
         override fun doInBackground(vararg params: String?): String? {
             var response: String?
             val url = "https://acnhapi.com/v1/fish"
             response = try {
-
-                URL(url).readText(
-                        Charsets.UTF_8
-                )
+                URL(url).readText(Charsets.UTF_8)
             } catch (e: Exception) {
                 null
             }
@@ -39,14 +36,15 @@ class MainActivity : AppCompatActivity() {
                 /* Extracting JSON returns from the API */
                 val jsonObj = JSONObject(result)
                 val jsonKeys = jsonObj.keys()
-                val fishItems = mutableListOf<JSONObject>()
+                val fishItems = mutableListOf<Fish>()
                 for (i in jsonKeys) {
                     val item = jsonObj.getJSONObject(i)
-                    fishItems.add(item)
+                    val availability = item.getJSONObject("availability")
+                    val months = availability["month-northern"].toString()
+                    val newFish = Fish(item["file-name"].toString(), item["price"].toString().toInt(), Availability(months, availability["location"].toString(), availability["rarity"].toString() ), item["image_uri"].toString())
+                    fishItems.add(newFish)
                 }
-                Log.d("post", "$fishItems")
                 mainText!!.text = fishItems.toString()
-
             } catch (e: Exception) {
                 Log.d("Post:", "error")
             }
